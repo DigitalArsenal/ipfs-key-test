0efee9be8539e5789ca54a53be024fd15f2a3611
tj <tj@digitalarsenal.io>
2022-03-04 12:37:48 -0500

    got it

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ import protobufjs from "protobufjs";
 import { existsSync } from "fs";
 import { readFile, writeFile } from "fs/promises";
 import crypto from "libp2p-crypto";
-import atob from "atob";
-import btoa from "btoa";
 import PeerId from "peer-id";
-
-import { base16 } from 'multiformats/bases/base16';
-import { base36 } from 'multiformats/bases/base36';
+import CID from 'cids';
 import { base58btc } from 'multiformats/bases/base58';
-import { identity } from 'multiformats/hashes/identity';
-
+import varint from "varint";
+import multihash from "multihashes";
 
 let { secp256k1 } = crypto.keys.supportedKeys;
 
@@ -26,8 +22,8 @@ if (!existsSync(kPath)) {
 let newKeyPairImported = await crypto.keys.import(await readFile(kPath, "utf-8"), "");
 
 
-await writeFile(pPath, crypto.keys.marshalPrivateKey(newKeyPairImported));
-await writeFile(pubPath, crypto.keys.marshalPublicKey(newKeyPairImported.public));
+await writeFile(pPath, crypto.keys.marshalPrivateKey(newKeyPairImported, "secp256k1"));
+await writeFile(pubPath, crypto.keys.marshalPublicKey(newKeyPairImported.public, "secp256k1"));
 
 let ipfsKey = await protobufjs.load('./key.proto');
 let protobufKey = await readFile(pPath);
@@ -38,22 +34,18 @@ delete results.KeyType;
 results["PublicKey"] = ipfsKey.lookupType("PublicKey").decode(protobufPubKey);
 results["PrivateKey"] = ipfsKey.lookupType("PrivateKey").decode(protobufKey);
 
-console.log(results)
+console.log("results", results)
 let pubKey = await secp256k1.unmarshalSecp256k1PublicKey(results.PublicKey.Data);
+let privKey = await secp256k1.unmarshalSecp256k1PrivateKey(results.PrivateKey.Data);
 
-console.log(pubKey.id);
+let myPeerID = PeerId.createFromPrivKey(protobufKey);
 
+const id = await privKey.id();
+
+console.log(varint.encode(pubKey.bytes.length * 8));
+console.log(Buffer.from(multihash.encode(pubKey.bytes, "identity")).toString('hex'))
+let myCID = new CID(1, "libp2p-key", multihash.encode(pubKey.bytes, "identity"));
+console.log(myCID.toString('base36'));
 //kzwfwjn5ji4puvokmcszflj3po708zngzw9h9wr2g5w0tcg4jo36i73kumah3i3
-/*
 
-console.log(Buffer.from(results.PrivateKey.Data).toString('hex'));
-
-
-
-const key = await secp256k1.unmarshalSecp256k1PrivateKey(protobufKey);
-const id = await key.id();
-console.log(id);
-console.log(base58btc.encode(Buffer.from(id)));
-//kzwfwjn5ji4pun29gq655o0bpfd89mg3ts36u5p65cza0pz0jzrb1prhlccczhs
-*/
 
